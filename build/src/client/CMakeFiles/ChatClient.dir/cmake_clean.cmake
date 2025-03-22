9f31895557ef5257479023965d5cd6bcac8757aa
xiaoan63 <3146653632@qq.com>
2025-03-22 21:28:39 +0800

    update the code

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/xaoan/projects/chat/chatserver/bin/ChatClient"
+  "/home/xaoan/projects/chat/chatserver/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
